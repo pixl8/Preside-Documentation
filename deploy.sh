557e6df79b74ef03670f13d8a27b85f9b6626dfc
Dominic Watson <watson.dominic@gmail.com>
2018-02-01 12:00:46 +0000

    Deploy from master OR develop

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,20 @@
-if [[ $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "master" ]] ; then
-  echo "Zipping up docs for offline download..."
-  cd builds/html
-  zip -q -r presidecms-docs.zip *
-  cd ../../
-  echo "Zipped."
-  echo "Preparing dash artifacts..."
-  cp -r builds/html builds/artifacts
-  mkdir builds/artifacts/dash
-  cp builds/dash/presidecms.xml builds/artifacts/dash/
-  cd builds/dash
-  tar -czf ../../builds/artifacts/dash/presidecms.tgz presidecms.docset
-  cd ../../
-  echo "Prepared."
-  echo "Syncing with S3..."
-  s3_website push
-  echo "All done :)"
+if [[ $TRAVIS_PULL_REQUEST == "false" ]] ; then
+  if [[ $TRAVIS_BRANCH == "master" || $TRAVIS_BRANCH == "develop" ]] ; then
+    echo "Zipping up docs for offline download..."
+    cd builds/html
+    zip -q -r presidecms-docs.zip *
+    cd ../../
+    echo "Zipped."
+    echo "Preparing dash artifacts..."
+    cp -r builds/html builds/artifacts
+    mkdir builds/artifacts/dash
+    cp builds/dash/presidecms.xml builds/artifacts/dash/
+    cd builds/dash
+    tar -czf ../../builds/artifacts/dash/presidecms.tgz presidecms.docset
+    cd ../../
+    echo "Prepared."
+    echo "Syncing with S3..."
+    s3_website push
+    echo "All done :)"
+  fi
 fi
