9152e3975b6a68e74825c827d83715ac595412ed
Dominic Watson <watson.dominic@gmail.com>
2018-02-01 12:13:25 +0000

    Dynamically set bucket based on build branch

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,12 @@
 if [[ $TRAVIS_PULL_REQUEST == "false" ]] ; then
   if [[ $TRAVIS_BRANCH == "master" || $TRAVIS_BRANCH == "develop" ]] ; then
+
+    if [[ $TRAVIS_BRANCH == "master" ]] ; then
+      S3_BUCKET="docs.preside.org"
+    else
+      S3_BUCKET="beta-docs.preside.org"
+    fi
+
     echo "Zipping up docs for offline download..."
     cd builds/html
     zip -q -r presidecms-docs.zip *
