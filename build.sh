820c762e7aa915961f26d304617002d3124024f1
Dominic Watson <watson.dominic@gmail.com>
2018-02-01 10:48:32 +0000

    Build changes to make project build by itslef on travis

--- build.sh
+++ build.sh
@@ -16,22 +16,4 @@ if [ -f .exitcode ]; then
   exit $exitcode
 fi
 
-echo "Building complete"
-if [[ $TRAVIS_TAG == v* ]] ; then
-  echo "Zipping up docs for offline download..."
-  cd builds/html
-  zip -q -r presidecms-docs.zip *
-  cd ../../
-  echo "Zipped."
-  echo "Preparing dash artifacts..."
-  cp -r builds/html builds/artifacts
-  mkdir builds/artifacts/dash
-  cp builds/dash/presidecms.xml builds/artifacts/dash/
-  cd builds/dash
-  tar -czf ../../builds/artifacts/dash/presidecms.tgz presidecms.docset
-  cd ../../
-  echo "Prepared."
-  echo "Syncing with S3..."
-  s3_website push
-  echo "All done :)"
-fi
+echo "Building complete."
