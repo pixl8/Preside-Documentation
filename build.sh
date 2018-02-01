92abac076344f6bacf0c2000a8745e856ec8c3f2
Dominic Watson <watson.dominic@gmail.com>
2018-02-01 13:17:39 +0000

    Set build env vars for diff branches

--- build.sh
+++ build.sh
@@ -3,6 +3,19 @@
 cd `dirname $0`
 CWD="`pwd`"
 
+EDIT_SOURCE_LINK=https://github.com/pixl8/Preside-Documentation/blob/master{path}
+DASH_BUILD_NUMBER=1.0.0
+DASH_DOWNLOAD_URL=http://docs.preside.org/dash/presidecms.tgz
+
+if [[ $TRAVIS_BRANCH == "develop" ]] ; then
+	EDIT_SOURCE_LINK=https://github.com/pixl8/Preside-Documentation/blob/develop{path}
+	DASH_DOWNLOAD_URL=http://beta-docs.preside.org/dash/presidecms.tgz
+fi
+
+export EDIT_SOURCE_LINK
+export DASH_BUILD_NUMBER
+export DASH_DOWNLOAD_URL
+
 echo "Building documentation with Lucee :)"
 
 box $CWD/build.cfm
