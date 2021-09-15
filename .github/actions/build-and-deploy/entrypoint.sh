8b07a1c7fd2bb8333fd130364a47a5bf574c2720
Dominic Watson <watson.dominic@gmail.com>
2021-09-15 14:41:16 +0100

    First stab at converting to github actions

---
+++ .github/actions/build-and-deploy/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cd ${GITHUB_WORKSPACE}
+
+box install save=false force=true || exit 1
+box start serverConfigFile=server.json || exit 1
+./generateDocs.sh || exit 1
+./build.sh || exit 1
+box stop serverConfigFile=server.json || exit 1
+./deploy.sh || exit 1
--- deploy.sh
+++ deploy.sh
@@ -1,28 +1,16 @@
-if [[ $TRAVIS_PULL_REQUEST == "false" ]] ; then
-  if [[ $TRAVIS_BRANCH == "master" || $TRAVIS_BRANCH == "develop" ]] ; then
-
-    if [[ $TRAVIS_BRANCH == "master" ]] ; then
-      S3_BUCKET="docs.preside.org"
-    else
-      S3_BUCKET="preside-beta-docs"
-    fi
-    export S3_BUCKET
-
-    echo "Zipping up docs for offline download..."
-    cd builds/html
-    zip -q -r presidecms-docs.zip *
-    cd ../../
-    echo "Zipped."
-    echo "Preparing dash artifacts..."
-    cp -r builds/html builds/artifacts
-    mkdir builds/artifacts/dash
-    cp builds/dash/presidecms.xml builds/artifacts/dash/
-    cd builds/dash
-    tar -czf ../../builds/artifacts/dash/presidecms.tgz presidecms.docset
-    cd ../../
-    echo "Prepared."
-    echo "Syncing with S3..."
-    s3_website push
-    echo "All done :)"
-  fi
-fi
+echo "Zipping up docs for offline download..."
+cd builds/html
+zip -q -r presidecms-docs.zip *
+cd ../../
+echo "Zipped."
+echo "Preparing dash artifacts..."
+cp -r builds/html builds/artifacts
+mkdir builds/artifacts/dash
+cp builds/dash/presidecms.xml builds/artifacts/dash/
+cd builds/dash
+tar -czf ../../builds/artifacts/dash/presidecms.tgz presidecms.docset
+cd ../../
+echo "Prepared."
+echo "Syncing with S3..."
+s3_website push
+echo "All done :)"
